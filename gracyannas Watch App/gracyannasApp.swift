987de7295a3b8d1ced20aaceb77b8292f8647991
Lucas <lucasrsvalenca@gmail.com>
2022-11-30 14:46:13 -0300

    create xcode projec

---
+++ gracyannas Watch App/gracyannasApp.swift
@@ -0,0 +1,17 @@
+//
+//  gracyannasApp.swift
+//  gracyannas Watch App
+//
+//  Created by lrsv on 30/11/22.
+//
+
+import SwiftUI
+
+@main
+struct gracyannas_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

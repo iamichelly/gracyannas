8fc5e2b00ff80fe9d7aecec4543525ac5f05fedc
Michelly <silva.michelly@gmail.com>
2022-12-01 00:06:24 -0300

    cria agendamento de notificação por botao temporário

--- gracyannas Watch App/ContentView.swift
+++ gracyannas Watch App/ContentView.swift
@@ -6,21 +6,75 @@
 //
 
 import SwiftUI
+import UserNotifications
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+
+    struct ContentView: View {
+        var body: some View {
+            VStack {
+                
+                Circle()
+                    .foregroundColor(.pink)
+                Text("Agende uma notificação em 5s:")
+                
+                Button("Agendar"){
+                    let content = UNMutableNotificationContent()
+                    content.title = "O de hoje tá pago?"
+                    content.body = "Antes o treino só valia com foto 📸, agora só vale se tu confirmar aqui embaixo:"
+                    content.sound = .defaultCritical
+                    content.categoryIdentifier = "myCategory"
+                    content.attachments = []
+                    
+
+//                    let attachement = try! UNNotificationAttachment(
+//                        identifier: "image",
+//                        url: Bundle.main.url(forResource: "//upload.wikimedia.org/wikipedia/commons/thumb/9/98/Andromeda_Galaxy_%28with_h-alpha%29.jpg/280px-Andromeda_Galaxy_%28with_h-alpha%29.jpg", withExtension: "jpg")!,
+//                        options: nil)
+//
+//                        content.attachments = [ attachement ]
+                    
+                    let action = UNNotificationAction(identifier: "my_action",
+                                                      title: "TÁ PAGO 💪",
+                                                      options: [])
+                    let anotherAction = UNNotificationAction(identifier: "another_action",
+                                                             title: "Tô devendo 😫",
+                                                             options: [])
+                    
+
+                    let category = UNNotificationCategory(identifier: "myCategory", actions: [action, anotherAction], intentIdentifiers: [], options: [])
+                    UNUserNotificationCenter.current().setNotificationCategories([category])
+                    
+                    
+
+                    let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+
+                    let request = UNNotificationRequest(identifier: "milk", content: content, trigger: trigger)
+                    
+                    UNUserNotificationCenter.current().add(request) { (error) in
+                        if let error = error{
+                            print(error.localizedDescription)
+                        }else{
+                            print("notif. agendada")
+                        }
+                    }
+                }
+                
+            }
+            .padding()
+            .onAppear(perform: { UNUserNotificationCenter.current().requestAuthorization(options: [.alert,.sound,.badge]) { (success, error) in
+                if success{
+                print("All set")
+                } else if let error = error {
+                print(error.localizedDescription)
+                }
+                }
+                
+            })
         }
-        .padding()
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
-}

4f4bc205d4ffbfcdf23a2683bc18d50c812e45a7
Lucas <lucasrsvalenca@gmail.com>
2022-12-04 13:14:48 -0300

    update user data

--- gracyannas Watch App/UserData.swift
+++ gracyannas Watch App/UserData.swift
@@ -4,37 +4,59 @@
 //
 //  Created by lrsv on 26/11/22.
 //
-
 import Foundation
 import SwiftUI
 
 class Storage: NSObject {
 
-static func archiveStringArray(object : [String]) -> Data {
-    do {
-        let data = try NSKeyedArchiver.archivedData(withRootObject: object, requiringSecureCoding: false)
-        return data
-    } catch {
-        fatalError("Can't encode data: \(error)")
+    static func archiveStringArray(object : [String]) -> Data {
+        do {
+            let data = try NSKeyedArchiver.archivedData(withRootObject: object, requiringSecureCoding: false)
+            return data
+        } catch {
+            fatalError("Can't encode data: \(error)")
+        }
+        
+    }
+
+    static func loadStringArray(data: Data) -> [String] {
+        do {
+            guard let array = try NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(data) as? [String] else {
+                return []
+            }
+            return array
+        } catch {
+            fatalError("loadWStringArray - Can't encode data: \(error)")
+        }
+    }
+    
+    static func archiveBooleanArray(object : [Bool]) -> Data {
+        do {
+            let data = try NSKeyedArchiver.archivedData(withRootObject: object, requiringSecureCoding: false)
+            return data
+        } catch {
+            fatalError("Can't encode data: \(error)")
+        }
+        
+    }
+
+    static func loadBooleanArray(data: Data) -> [Bool] {
+        do {
+            guard let array = try NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(data) as? [Bool] else {
+                return []
+            }
+            return array
+        } catch {
+            fatalError("loadWStringArray - Can't encode data: \(error)")
+        }
     }
     
 }
 
-static func loadStringArray(data: Data) -> [String] {
-    do {
-        guard let array = try NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(data) as? [String] else {
-            return []
-        }
-        return array
-    } catch {
-        fatalError("loadWStringArray - Can't encode data: \(error)")
-    }
-}}
-
-class UserDataController: ObservableObject {
-    @State var trainingDays: [Bool] = []
-    @State var trainingTypePerDay: [String] = []
-    @State var trainingHistory: [String] = []
+class UserData: ObservableObject {
+    @Published var trainingDays: [Bool] = [false, false, false, false, false, false, false]
+    @Published var trainingTypePerDay: [String] = []
+    @Published var trainingHistory: [String] = []
     @AppStorage("trainingAmountPerWeek") var trainingAmountPerWeek: Int = 0
     @AppStorage("trainingsCompletedThisWeek") var trainingsCompletedThisWeek: Int = 0
     @AppStorage("trainingDays") var trainingDaysData: Data = Data()
